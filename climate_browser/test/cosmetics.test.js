ff8769814b8a863ea53e13c139cbd18e721a1075
Janos Tordai <tjanos88@gmail.com>
2023-10-25 07:30:59 +0000

    TST: added more tests

--- climate_browser/test/cosmetics.test.js
+++ climate_browser/test/cosmetics.test.js
@@ -1,9 +1,10 @@
 import { describe, expect, it } from "@jest/globals";
+import "@testing-library/jest-dom";
 
-import { getNumberWithSign } from "../src/utils/cosmetics";
+import { getNumberWithSign, activateButton } from "../src/utils/cosmetics";
 
-describe("Converting numeric value to string", () => {
-  it("Takes 5 as input and returns +5 as string", () => {
+describe("Testing getNumberWithSign", () => {
+  it("takes 5 as input and returns +5 as string", () => {
     const input = 5;
     const output = "+5";
     expect(getNumberWithSign(input)).toEqual(output);
@@ -18,5 +19,23 @@ describe("Converting numeric value to string", () => {
     const input = 0;
     const output = "0";
     expect(getNumberWithSign(input)).toEqual(output);
+  });
+});
+
+describe("Testing activateButton", () => {
+  it("should activate the button when the container exists", () => {
+    document.body.innerHTML = `
+      <div class="button-container">
+        <button id="button1" class="outline">Button 1</button>
+        <button id="button2" class="outline">Button 2</button>
+        <button id="button3" class="outline">Button 3</button>
+      </div>
+    `;
+
+    activateButton("button-container", "button2");
+
+    expect(
+      document.getElementById("button2").classList.contains("outline")
+    ).toBe(false);
   });
 });
