d5153f1a7eb45aa1186d61650ac6bebdf01571d9
Janos Tordai <tjanos88@gmail.com>
2023-10-25 07:30:28 +0000

    DOC: added documentation

--- climate_browser/src/utils/cosmetics.js
+++ climate_browser/src/utils/cosmetics.js
@@ -1,15 +1,31 @@
-export function getNumberWithSign(theNumber) {
-  if (theNumber > 0) {
-    return `+${theNumber}`;
+/**
+ * Given a number, return a string with a "+" or "-" prefix, or simply 0 if number is zero.
+ * @param {numeric} number
+ * @returns {string}
+ */
+
+export function getNumberWithSign(number) {
+  if (number > 0) {
+    return `+${number}`;
   }
-  return theNumber.toString();
+  return number.toString();
 }
 
-export function activateButton(divClass, id) {
-  const element = document.getElementById(id);
-  const timelineButtons = document.querySelectorAll(`.${divClass} button`);
-  timelineButtons.forEach((button) => {
+/**
+ * Activate a specific button in a container by removing the "outline" class from it
+ * and adding the "outline" class to all other buttons in the container.
+ *
+ * @param {string} containerSelector
+ * @param {string} buttonId
+ * @returns
+ */
+
+export function activateButton(containerSelector, buttonId) {
+  const container = document.querySelectorAll(`.${containerSelector} button`);
+  const buttons = document.getElementById(buttonId);
+
+  container.forEach((button) => {
     button.classList.add("outline");
   });
-  element.classList.remove("outline");
+  buttons.classList.remove("outline");
 }
