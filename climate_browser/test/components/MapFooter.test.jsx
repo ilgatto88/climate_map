c0e7e8b2ab454104eaa77a1cf258b91e6de9c951
Janos Tordai <tjanos88@gmail.com>
2023-11-07 21:50:36 +0000

    TST: fixed test after refactoring

--- climate_browser/test/components/MapFooter.test.jsx
+++ climate_browser/test/components/MapFooter.test.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { describe, expect, it, beforeEach } from "@jest/globals";
 import configureStore from "redux-mock-store";
 import MapFooter from "../../src/components/MapFooter";
-import { changeCategory } from "../../src/features/categorySlice";
 import "@testing-library/jest-dom";
 
 const mockStore = configureStore();
@@ -45,23 +44,5 @@ describe("MapFooter Component", () => {
       );
       expect(categoryItem.length).toBe(1);
     });
-  });
-
-  it("dispatches changeCategory when a category is clicked", () => {
-    const { container } = render(
-      <Provider store={store}>
-        <MapFooter />
-      </Provider>
-    );
-
-    // Click on a category item
-    const categoryItem = container.getElementsByClassName(
-      "category-temperature"
-    )[0];
-    fireEvent.click(categoryItem);
-
-    // Assert that the changeCategory action is dispatched with the correct category name
-    const actions = store.getActions();
-    expect(actions[0]).toEqual(changeCategory("temperature"));
   });
 });
