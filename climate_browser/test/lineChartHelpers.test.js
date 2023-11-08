b8f0d098d1e44771ffb91443918a57d09d998b75
Janos Tordai <tjanos88@gmail.com>
2023-11-08 18:29:16 +0000

    TST: added more tests

--- climate_browser/test/lineChartHelpers.test.js
+++ climate_browser/test/lineChartHelpers.test.js
@@ -4,6 +4,7 @@ import "@testing-library/jest-dom";
 import {
   lineChartAnnotationPaddingCalculator,
   insertNullValues,
+  range,
 } from "../src/utils/lineChartHelpers";
 
 describe("Testing lineChartAnnotationPaddingCalculator", () => {
@@ -46,7 +47,7 @@ describe("Testing insertNullValues", () => {
     expect(result).toEqual(expectedOutput);
   });
 
-  it("takes an array with one missing element and returns the same array", () => {
+  it("takes an array with one missing element and returns the array with added null", () => {
     const input = [10, 20, 30];
     const expectedOutput = [null, 10, 20, 30];
     const timeRange = [2020, 2021, 2022];
@@ -55,5 +56,26 @@ describe("Testing insertNullValues", () => {
 
     const result = insertNullValues(input, timeRange, startYear, endYear);
     expect(result).toEqual(expectedOutput);
+  });
+
+  it("takes an array with multiple elements missing at the end and returns the extended array", () => {
+    const input = [10, 20, 30];
+    const expectedOutput = [10, 20, 30, null, null, null];
+    const timeRange = [2020, 2021, 2022];
+    const startYear = 2020;
+    const endYear = 2025;
+
+    const result = insertNullValues(input, timeRange, startYear, endYear);
+    expect(result).toEqual(expectedOutput);
+  });
+});
+
+describe("Testing custom range function", () => {
+  it("start and end year and returns array filled with all years in between", () => {
+    const startYear = 2000;
+    const endYear = 2005;
+    const expectedOutput = [2000, 2001, 2002, 2003, 2004, 2005];
+    const output = range(startYear, endYear);
+    expect(output).toEqual(expectedOutput);
   });
 });
