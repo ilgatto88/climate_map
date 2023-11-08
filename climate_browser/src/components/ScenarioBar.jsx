3234ffe328d084568e45c6767a4d3f9d3226d7b5
Janos Tordai <tjanos88@gmail.com>
2023-11-08 08:47:58 +0000

    REF: refactored repeating parts, renamed some css classes, added new parameters to data files

--- climate_browser/src/components/ScenarioBar.jsx
+++ climate_browser/src/components/ScenarioBar.jsx
@@ -6,74 +6,71 @@ import { activateButton } from "../utils/cosmetics";
 import { changeScenario } from "../features/variables/scenarioSlice";
 import { changeFuturePeriod } from "../features/variables/futurePeriodSlice";
 
+import SCENARIOS_JSON from "../data/scenarios.json";
+import TIMEPERIOD_JSON from "../data/timeperiod.json";
+
+const SCENARIOS = Object.keys(SCENARIOS_JSON).map((key) => SCENARIOS_JSON[key]);
+const TIMEPERIODS = Object.keys(TIMEPERIOD_JSON).map(
+  (key) => TIMEPERIOD_JSON[key]
+);
+
 function ScenarioBar() {
   const dispatch = useDispatch();
+
+  const handleScenarioButtonClick = (scenario, buttonId) => {
+    dispatch(changeScenario(scenario));
+    activateButton("scenario-buttons", buttonId);
+  };
+
+  const handleFuturePeriodButtonClick = (period, periodName) => {
+    dispatch(changeFuturePeriod(period));
+    activateButton("timeperiod-buttons", periodName);
+  };
+
   return (
-    <div className="scenario-timeline-bar-container">
-      <div className="scenario-timeline-bar">
+    <div className="scenario-timeperiod-bar-container">
+      <div className="scenario-timeperiod-bar">
         <div className="scenario-col">
-          <div className="scenario-timeline-title">Climate Change</div>
+          <div className="scenario-timeperiod-title">Climate Change</div>
           <div className="scenario-buttons">
-            <button
-              type="button"
-              id="rcp26Button"
-              className="scenario-timeline-buttons outline"
-              onClick={() => {
-                dispatch(changeScenario("rcp26"));
-                activateButton("scenario-buttons", "rcp26Button");
-              }}
-            >
-              Less
-            </button>
-            <button
-              type="button"
-              id="rcp85Button"
-              className="scenario-timeline-buttons"
-              onClick={() => {
-                dispatch(changeScenario("rcp85"));
-                activateButton("scenario-buttons", "rcp85Button");
-              }}
-            >
-              More
-            </button>
+            {SCENARIOS.map((scenario) => (
+              <button
+                key={scenario.id}
+                type="button"
+                id={`${scenario.lowerCaseName}Button`}
+                className={`scenario-timeperiod-buttons ${
+                  scenario.buttonOutline ? "outline" : ""
+                }`}
+                onClick={() =>
+                  handleScenarioButtonClick(
+                    scenario.lowerCaseName,
+                    `${scenario.lowerCaseName}Button`
+                  )
+                }
+              >
+                {scenario.buttonName}
+              </button>
+            ))}
           </div>
         </div>
         <div className="timeline-col">
-          <div className="scenario-timeline-title">Time Period</div>
-          <div className="timeline-buttons">
-            <button
-              type="button"
-              id="nearPast"
-              className="scenario-timeline-buttons outline"
-              onClick={() => {
-                dispatch(changeFuturePeriod("1991-2020"));
-                activateButton("timeline-buttons", "nearPast");
-              }}
-            >
-              1991-2020
-            </button>
-            <button
-              type="button"
-              id="nearFuture"
-              className="scenario-timeline-buttons"
-              onClick={() => {
-                dispatch(changeFuturePeriod("2036-2065"));
-                activateButton("timeline-buttons", "nearFuture");
-              }}
-            >
-              2036-2065
-            </button>
-            <button
-              type="button"
-              id="distantFuture"
-              className="scenario-timeline-buttons outline"
-              onClick={() => {
-                dispatch(changeFuturePeriod("2071-2100"));
-                activateButton("timeline-buttons", "distantFuture");
-              }}
-            >
-              2071-2100
-            </button>
+          <div className="scenario-timeperiod-title">Time Period</div>
+          <div className="timeperiod-buttons">
+            {TIMEPERIODS.map((timeperiod) => (
+              <button
+                key={timeperiod.id}
+                type="button"
+                id={timeperiod.id}
+                className={`scenario-timeperiod-buttons ${
+                  timeperiod.buttonOutline ? "outline" : ""
+                }`}
+                onClick={() =>
+                  handleFuturePeriodButtonClick(timeperiod.years, timeperiod.id)
+                }
+              >
+                {timeperiod.years}
+              </button>
+            ))}
           </div>
         </div>
       </div>
